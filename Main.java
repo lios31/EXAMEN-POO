6d77afe97f25134e0e78f88cb99539a325aa408a
Jorge_Mindiola <146681676+lios31@users.noreply.github.com>
2024-12-07 13:15:31 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import controlador.Juego;
+
+public class Main {
+    public static void main(String[] args) {
+        Juego juego = new Juego();
+        juego.iniciar();
+    }
+
+	public static boolean checkGameOver(String string) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static boolean checkWinningCondition() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static void initializeGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static boolean validateInput(String input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
